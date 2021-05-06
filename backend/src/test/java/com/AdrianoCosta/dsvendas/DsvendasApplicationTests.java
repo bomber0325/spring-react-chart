10cfa482fb61bbc1be2f774ace56be7c3a19d2ee
ericdev0212 <eric.devos0212@gmail.com>
2021-05-06 11:45:24 -0300

    Initial Commit

---
+++ backend/src/test/java/com/AdrianoCosta/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdrianoCosta.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="footer mt-auto py-3 bg-dark">
+    <div className="container">
+      <p className="text-light">
+        App desenvolvido por{' '}
+        <a
+          href="https://github.com/adrianocostajhp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Adriano Costa
+        </a>
+      </p>
+      <p className="text-light">
+        <small>
+          <strong>Semana Spring React</strong>
+          <br />
+          Evento promovido pela escola DevSuperior:{' '}
+          <a
+            href="https://instagram.com/devsuperior.ig"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @devsuperior.ig
+          </a>
+        </small>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
